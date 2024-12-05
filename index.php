feefec5789b3a65795d353743a11afafdf16d156
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2024-12-05 15:23:49 -0600

    Updated bootstrap to version 5

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1"><!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title><?php echo $title; ?></title>
 
@@ -24,7 +24,7 @@
 
 	<link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="vendor/select2/select2/dist/css/select2.min.css" rel="stylesheet" type="text/css">
-	<link href="vendor/intelogie/select2-bootstrap-theme/dist/select2-bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="vendor/apalfrey/select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css">
 	<link href="includes/css/main.inc.css" rel="stylesheet" type="text/css">
 
 </head>
@@ -57,54 +57,60 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<div class="form-horizontal">
 					<fieldset>
-						<legend>Choose a Queue</legend>
+						<legend>Choose a Partition</legend>
 						<?php
 							$checked = " checked";
 							foreach($config['queues'] as $queue){
-								echo '<div class="form-group"><label class="col-sm-4">'.$queue['name'].' queue</label> <div class="col-sm-8"><input type="radio" class="queue_radio" name="queue" value="'.$queue['name'].'"'.$checked.' onchange="generateScript();" /> </div></div>';
+								echo "<div class='form-check'>";
+								echo "<input type='radio' class='queue_radio form-check-input' name='queue' value='" . $queue['name'] . "'" . $checked . " onchange='generateScript();' />";
+								echo "&nbsp;<label class='form-check-label'>" . $queue['name'] . "</label>";
+								echo "</div>";
 								$checked = "";
 							}
 						?>
 					</fieldset>
 					<fieldset>
 						<legend>Allocate Resources</legend>
-						<div class="form-group">
+						<div class="row">
 							<label class="col-sm-4">CPU (cores)</label>
 							<div class="col-sm-8">
-								<select id="cpu" class="form-control select2_dropdown" onchange="generateScript();">
+								<select id="cpu" class="form-select select2_dropdown" onchange="generateScript();">
 								</select>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Memory (GB)</label>
 							<div class="col-sm-8">
-								<select id="memory" class="form-control select2_dropdown" onchange="generateScript();">
+								<select id="memory" class="form-select select2_dropdown" onchange="generateScript();">
 								</select>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Nodes</label>
 							<div class="col-sm-8">
-								<select id="nodes" class="form-control select2_dropdown" onchange="generateScript();">
+								<select id="nodes" class="form-select select2_dropdown" onchange="generateScript();">
 								</select>
 							</div>
 						</div>
-						<div class="form-group" id='gpu-group'>
+						&nbsp;
+						<div class="row" id='gpu-group'>
 							<label class="col-sm-4">GPUs</label>
 							<div class="col-sm-8">
-								<select id="gpu" class="form-control select2_dropdown" onchange="generateScript();">
+								<select id="gpu" class="form-select select2_dropdown" onchange="generateScript();">
 								</select>
 							</div>
 						</div>
 					</fieldset>
+					&nbsp;
 					<fieldset>
 						<legend>Modules/Commands</legend>
-						<div class="form-group">
+						<div class="row">
 							<label class="col-sm-4">Modules to load</label>
 							<div class="col-sm-8">
-								<select id='modules' multiple class='select2_dropdown form-control' onchange="generateScript();">
+								<select id='modules' multiple class='select2_dropdown form-select' onchange="generateScript();">
 									<?php
 										$lines = preg_split('/ +/',file_get_contents($config['config']['apps_url']));
 										$software = array();
@@ -119,14 +125,15 @@
 									    
 									    foreach( $software as $module){
 									        if(! (preg_match('/modules|^-|^ -|\/$/',$module)) ){
-												echo "<option value='{$module}' >{$module}</option>";
+												echo "<option value='{$module}' >{$module}</option>\n";
 									        }
 									    }
 									    ?>
 								</select>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Commands to run</label>
 							<div class="col-sm-8">
 								<textarea class="form-control" id="commands" rows="2" placeholder="formatdb -p F -i all_seqs.fasta -n customBLASTdb" onkeyup="generateScript();"></textarea>
@@ -135,64 +142,65 @@
 					</fieldset>
 					<fieldset>
 						<legend>Recommended Settings</legend>
-						<div class="form-group">
+						<div class="row">
 							<label class="col-sm-4">Email</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="email" id="email" onkeyup="generateScript();"/>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Job Name</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="jobname" id="jobname" onkeyup="generateScript();"/>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Working directory</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="workingdir" id="workingdir" onkeyup="generateScript();" placeholder="/full/path/to/directory/"/>
 							</div>
 						</div>
 					</fieldset>
-				</div>
 			</div>
 			<div class="col-sm-6">
-				<div class="form-horizontal">
 					<fieldset>
 						<legend>SLURM Script</legend>
 						<textarea id="slurm" class="form-control" rows="16" readonly></textarea>
 					</fieldset>
 					<fieldset>
 						<legend>Optional Settings</legend>
-						<div class="form-group">
+						<div class="row">
 							<label class="col-sm-4">Std out file</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="stdout" id="stdout" onkeyup="generateScript();"/>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Std err file</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="stderr" id="stderr" onkeyup="generateScript();"/>
 							</div>
 						</div>
-						<div class="form-group">
+						&nbsp;
+						<div class="row">
 							<label class="col-sm-4">Project</label>
 							<div class="col-sm-8">
 								<input type="text" class="form-control" name="project" id="project" onkeyup="generateScript();"/>
 							</div>
 						</div>
 					</fieldset>
-				</div>
 			</div>
 		</div>
 	</div>
-	
+	&nbsp;	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script type="text/javascript">
-		$.fn.select2.defaults.set( "theme", "bootstrap" );
-		$.fn.select2.defaults.set( "width", null );
+		$.fn.select2.defaults.set( "theme", "bootstrap-5" );
+		$.fn.select2.defaults.set( "width", "resolve" );
 		
 		$('.queue_radio').on('change',populateResourceDropdowns);
 		
